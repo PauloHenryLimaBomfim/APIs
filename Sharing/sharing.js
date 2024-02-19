b9928fd7db2bbb7a55fda9691bdbeb11f643a834
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2024-02-19 10:11:02 -0300

    substituição conteudo -> subtitulo, adicionando texto no compartilhamento

--- Sharing/sharing.js
+++ Sharing/sharing.js
@@ -2,6 +2,7 @@ function compartilhar(){
     if (navigator.share) {
         navigator.share({
           title: document.getElementById('tituloPrincipal').innerText ,
+          text: document.getElementById('subtitulo').innerText,
           url: 'http://127.0.0.1:5500/Sharing/index.html'
         }).then(() => {
           console.log('Compartilhado com sucesso!');
