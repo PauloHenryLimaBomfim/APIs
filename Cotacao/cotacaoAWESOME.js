ce4fdd70c2426fada481e916ecb35e1a2898e0a0
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2024-02-19 08:04:31 -0300

    Euro funcionando...

--- Cotacao/cotacaoAWESOME.js
+++ Cotacao/cotacaoAWESOME.js
@@ -3,14 +3,30 @@
 const obterDados=()=>{
     const endpoint = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL";
     fetch(endpoint).then(res=>res.json()).then(dados=>{
-    let setaDolar = document.getElementById('setaDolar');
-    let setaEuro = document.getElementById('setaEuro');
-    let valorDolar = document.getElementById('valorDolar');
-    let valorEuro = document.getElementById('valorEuro');
-    console.log(dados);
-    valorDolar.innerHTML = ((Number(dados.USDBRL.ask) + Number(dados.USDBRL.bid)) / 2).toFixed(2);
-    valorEuro.innerHTML = ((Number(dados.EURBRL.ask) + Number(dados.USDBRL.bid)) / 2).toFixed(2);
+        let setaDolar = document.getElementById('setaDolar');
+        let setaEuro = document.getElementById('setaEuro');
+        let valorDolar = document.getElementById('valorDolar');
+        let valorEuro = document.getElementById('valorEuro');
+
+        valorDolar.innerHTML = ((Number(dados.USDBRL.ask) + Number(dados.USDBRL.bid)) / 2).toFixed(2);
+        valorEuro.innerHTML = ((Number(dados.EURBRL.ask) + Number(dados.EURBRL.bid)) / 2).toFixed(2);
+
+        if(dados.USDBRL.varBid >= 0){
+            valorDolar.style.color = '#0ed145';
+        }
+        else{
+            setaDolar.src = "SetaVermelha.png";
+            valorDolar.style.color = '#d41920';
+        }
+
+        if(dados.EURBRL.varBid >= 0){
+            valorEuro.style.color = '#0ed145';
+        }
+        else{
+            setaEuro.src = "SetaVermelha.png";
+            valorEuro.style.color = '#d41920';
+        }
+        console.log(dados.EURBRL.varBid);
     });
 }
-
-let intervalo = setInterval(obterDados(), 30000);
+setInterval(obterDados(), 30000);
