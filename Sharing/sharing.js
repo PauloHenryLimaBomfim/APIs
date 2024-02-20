89f15849a0d9ea9c8f60ee26cce6a7811fd09637
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2024-02-20 11:24:02 -0300

    Copiar para area de transferencia e twettar funcionando

--- Sharing/sharing.js
+++ Sharing/sharing.js
@@ -12,3 +12,17 @@ function compartilhar(){
         
     }
 }
+
+function carregarLinks(){
+  let link = encodeURI(window.location.href);
+  let titulo = encodeURIComponent(document.getElementById('tituloPrincipal').innerText);
+  let twitter = document.getElementById('twitterShareButton');
+
+  twitter.href = `http://twitter.com/share?&url=${link}&text=${titulo}`; 
+  link.select();
+}
+function copiarAreaTransferencia(){
+  let link = encodeURI(window.location.href);
+  navigator.clipboard.writeText(link);
+  alert('Copiado para Area de Transferência')
+}
