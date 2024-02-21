1697c94594adafbc15022071bed85d7422c92c13
PauloHenryLimaBomfim <zzHenryx@gmail.com>
2024-02-21 10:24:50 -0300

    Todos os compartilhamentos funcionando

--- Sharing/sharing.js
+++ Sharing/sharing.js
@@ -1,4 +1,5 @@
-function compartilhar(){
+
+function compartilhar(){ // Compartilhar para dispositivos moveis
     if (navigator.share) {
         navigator.share({
           title: document.getElementById('tituloPrincipal').innerText ,
@@ -17,12 +18,17 @@ function carregarLinks(){
   let link = encodeURI(window.location.href);
   let titulo = encodeURIComponent(document.getElementById('tituloPrincipal').innerText);
   let twitter = document.getElementById('twitterShareButton');
+  let facebook = document.getElementById('facebookShareButton');
+  let whatsapp = document.getElementById('whatsappShareButton');
 
-  twitter.href = `http://twitter.com/share?&url=${link}&text=${titulo}`; 
-  link.select();
+  twitter.href = `http://twitter.com/share?&url=${link}&text=${titulo}`;
+  facebook.href = `https://www.facebook.com/sharer/sharer.php?u=${link}`;
+  whatsapp.href = `https://api.whatsapp.com/send?text=${titulo}%20${link}`;
 }
+
 function copiarAreaTransferencia(){
   let link = encodeURI(window.location.href);
   navigator.clipboard.writeText(link);
   alert('Copiado para Area de Transferência')
 }
+
